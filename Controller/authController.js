7c0869680f86a4d40c04bf5d4822b78cbbdac4a0
Urlgrim <21522752@gm.uit.edu.vn>
2024-11-19 10:34:03 +0700

    fix connection

--- Controller/authController.js
+++ Controller/authController.js
@@ -23,7 +23,7 @@ const createSendToken = (customer, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: "lax",
   };
 
