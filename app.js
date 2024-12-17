4bb6141789d6349359c9c66ee1fba796eaea8f7f
Urlgrim <147356147+Urlgrim@users.noreply.github.com>
2024-12-18 01:16:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(morgan("common"));
 app.use(
   cors({
     origin: [
+      "http://192.168.1.47/80",
       "http://127.0.0.1:3000",
       "http://127.0.0.1:5000",
       "http://localhost:3000",
